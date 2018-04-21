2759fe1711a86d990eb78b967947b6c5c79ac4dd
kildares <kildare.silveira@gmail.com>
2018-04-21 13:47:28 -0300

    ADD functionality to fetch web pages by depth

--- src/org/com/kildare/crawler/org/com/kildare/crawler/web/WebCrawler.java
+++ src/org/com/kildare/crawler/org/com/kildare/crawler/web/WebCrawler.java
@@ -1,5 +1,13 @@
 package org.com.kildare.crawler.org.com.kildare.crawler.web;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.ConnectException;
+
 public class WebCrawler {
 
     public static void crawl(String url, int depth) throws UnsupportedOperationException {
@@ -8,18 +16,31 @@ public class WebCrawler {
             throw new UnsupportedOperationException();
         }
 
-        //TODO fetch web page content
-        //TODO obtain web page hiperlinks
-        //TODO print web page content
-
-        if(depth > 0){
+        try {
+            System.out.println("Retrieving web page: " + url);
+            Document doc = Jsoup.connect(url).get();
+            String text = doc.text();
+            System.out.println(text);
+            System.out.println("End retrieving web page");
+            Elements links = doc.select("a[href]");
             depth--;
-            try{
-                crawl(url, depth);
-            }catch(UnsupportedOperationException e){
-                e.printStackTrace();
+            if(depth >= 0)
+            {
+                for(Element link : links)
+                {
+                    try{
+                        String nextUrl = link.attr("abs:href").toString();
+                        crawl(nextUrl, depth);
+                    }catch(UnsupportedOperationException e){
+                        break;
+                    }
+                }
             }
 
+        }catch (ConnectException e){
+            System.out.println("Connection refused to url: " + url);
+        }catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
